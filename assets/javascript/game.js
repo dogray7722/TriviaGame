b50054d5124136980bb69af21b9b4e9276557d9d
Dave Gray <davegray@Daves-MacBook-Air.local>
2017-06-01 06:26:17 -0600

    Create initial directories

---
+++ assets/javascript/game.js
@@ -0,0 +1,93 @@
+//Document function
+$(document).ready(function() {
+
+//Objects containing trivia questions and answers
+
+var worldHistory = {
+	question1: "",
+	answerA: "", 
+	answerB: "",
+	answerC: "",
+	answerD: "",
+
+	question2: "",
+	answerA: "", 
+	answerB: "",
+	answerC: "",
+	answerD: "",
+
+	question3: "",
+	answerA: "", 
+	answerB: "",
+	answerC: "",
+	answerD: "",
+}
+
+var geograpy = {
+	question1: "Which country has legislative authority over Greenland?",
+	answerA: "USA", 
+	answerB: "Iceland",
+	answerC: "Denmark",
+	answerD: "Sweden",
+
+	question2: "Which four countries border the Czech Republic?",
+	answerA: "Germany, Poland, Austria, Slovenia", 
+	answerB: "Germany, Poland, Austria, Slovakia",
+	answerC: "Germany, Poland, Hungary, Austria",
+	answerD: "Germany, Slovenia, Slovakia, Hungary",
+
+	question3: "",
+	answerA: "", 
+	answerB: "",
+	answerC: "",
+	answerD: "",,
+
+}
+
+var world languages = {
+	question1: "",
+	answer1: "",
+
+	question2: "",
+	answer2: "",
+
+	question3: "",
+	answer3: ""
+}
+
+var world foods = {
+	question1: "",
+	answer1: "",
+
+	question2: "",
+	answer2: "",
+
+	question3: "",
+	answer3: ""
+}
+
+var countDown = 30;
+var score = 0;
+var guessesLeft = 3;
+
+//Geography Button
+$("#geography").on("click", function() {
+
+	});
+
+//World History Button
+$("#worldhistory").on("click", function() {
+
+	});
+
+$("#worldlanguages").on("click", function() {
+
+	});
+
+$("#worldfoods").on("click", function() {
+
+	});
+
+
+
+});
