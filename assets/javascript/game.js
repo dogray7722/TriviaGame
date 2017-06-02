05e2df3631918d7b9745d18072defd067a821315
Dave Gray <davegray@Daves-MacBook-Air.local>
2017-06-01 23:04:42 -0600

    fleshing out functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,11 +41,13 @@ var geograpy = {
 	answerC: "Lagos",
 	answerD: "Yaounde",
 
-	question4: "Which river is the shortest?"
-	answerA: "Yangtze"
-	answerB: "Yellow"
-	answerC: "Parana"
-	answerD: "Congo"
+	question4: "Which river is the shortest?",
+	answerA: "Yangtze",
+	answerB: "Yellow",
+	answerC: "Parana",
+	answerD: "Congo",
+
+	answers: ["c","b","a","d"];
 
 }
 
@@ -71,41 +73,98 @@ var world foods = {
 	answer3: ""
 }
 
-var countDown;
 var correctAnswers;
 var incorrectAnswers;
 var answersGuessed = [];
+var time = 0;
+var clockRunning = false;
+var intervalID;
 
 
-	function timer() {
-	//Create the timer div, pop it into the html,  and start the clock at 1:30 
-	//Start the countdown
+	function reset () {
+
+	//Reset the clock to 1:30
+
+	time = 0;
+	$('#questions').empty();
+    $('#questions').text("01:30");
+
+	}
+
+	function timerStart() {
+	
+	//Create the timer div, pop it into the html,  and start the clock at 1:30
+
+    if(!clockRunning) {
+    	intervalID = setInterval(count, 1000);
+    	}
+
 	//If the value of the timer reaches zero, call the results function
+	
 	};
+
+	function stop() {
+	clearInterval(stop);
+	clockRunning = false;
+
+	};
+
+	function count() {
+		time--;
+
+		var counter = convertTime(time);
+
+		$('#questions').html(counter);
+	}; 
 
 	function results() {
 	//Compare the answers guessed to the correct answers
+
+	for (i = 0; i < answerArray.length) {
+		if 
+	}
+
 	//Increment correct answers by one if the answer is correct, increment incorrect answers by one if the answer is incorrect
 	//Create the results div, pop it into the html
 
 	};
 
-	function convertTime() {
+	function convertTime(t) {
 	//Steal the code from the stopwatch activity to convert time	
-	}
+	var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  	}
 
 	//Geography Button
 	$("#geography").on("click", function() {
 	//Show the geography questions
-	
+
 	//Show the answer choices as radio buttons
 	//Don't allow selection of more than one radio button
 	//Call the timer function
+	timerStart();
+
 	});
 
 	//World History Button
 	$("#worldhistory").on("click", function() {
+	
 	//Show the world history questions
+
 	//Show the answer choices as radio buttons
 	//Don't allow selection of more than one radio button
 	//Call the timer function
